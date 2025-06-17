783ec33694f2f1289e24f4f80341bd46bebe0705
Sajib12Saha <sahasojib0155@gmail.com>
2025-06-17 14:14:26 +0600

    sffs

--- components/form/blog-form.tsx
+++ components/form/blog-form.tsx
@@ -168,7 +168,7 @@ export const Blogform = ({ defaultValue, onCancel }: Props) => {
                   </div>
                 </FormLabel>
 
-            <div className="w-full">
+            <div className="w-full overflow-hidden">
    <RichTextEditor
                     value={form.watch("content")}
                     onChange={handleContentChange}
