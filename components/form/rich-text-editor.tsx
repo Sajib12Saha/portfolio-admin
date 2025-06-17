193e8000b8f6f49c1b54780b22e7e4eeb57f48cd
Sajib12Saha <sahasojib0155@gmail.com>
2025-06-17 13:54:13 +0600

    fsdf

--- components/form/rich-text-editor.tsx
+++ components/form/rich-text-editor.tsx
@@ -224,7 +224,7 @@ export const RichTextEditor = ({
     'prose prose-sm w-full rounded-md border px-3  py-2 text-sm bg-input text-foreground shadow-[inset_3px_3px_3px_rgba(0,0,0,0.25),inset_-1px_-1px_4px_rgba(255,255,255,0.8)] dark:shadow-[inset_3px_3px_3px_rgba(0,0,0,0.25),inset_-1px_-1px_4px_rgba(255,255,255,0.16)]'
   )}
 >
-  <EditorContent editor={editor} />
+  <EditorContent editor={editor} className='w-full'/>
 </div>
     </div>
   );
