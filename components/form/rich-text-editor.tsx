cf9570ea4c738007a58e3588b5fe8179e701e62f
Sajib12Saha <145220509+Sajib12Saha@users.noreply.github.com>
2025-06-19 15:29:08 +0600

    Update rich-text-editor.tsx

--- components/form/rich-text-editor.tsx
+++ components/form/rich-text-editor.tsx
@@ -91,7 +91,7 @@ export const RichTextEditor = ({
   return (
     <div className="space-y-2">
       {/* Toolbar */}
-      <div className="flex flex-wrap gap-2 sm:gap-1 px-3 py-2 rounded-md bg-input overflow-x-hidden shadow-[inset_3px_3px_3px_rgba(0,0,0,0.25),inset_-1px_-1px_4px_rgba(255,255,255,0.8)] dark:shadow-[inset_3px_3px_3px_rgba(0,0,0,0.25),inset_-1px_-1px_4px_rgba(255,255,255,0.16)]">
+      <div className="flex flex-wrap gap-2 md:gap-4 px-3 py-2 rounded-md bg-input overflow-x-hidden shadow-[inset_3px_3px_3px_rgba(0,0,0,0.25),inset_-1px_-1px_4px_rgba(255,255,255,0.8)] dark:shadow-[inset_3px_3px_3px_rgba(0,0,0,0.25),inset_-1px_-1px_4px_rgba(255,255,255,0.16)]">
         <Button
           type="button"
           size="sm"
