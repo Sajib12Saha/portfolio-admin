dbf09b210b8a2f34f12a3993257ba651a99cc4f0
Sajib12Saha <145220509+Sajib12Saha@users.noreply.github.com>
2025-06-19 14:55:51 +0600

    Update blog-table.tsx

--- components/data-table/blog-table.tsx
+++ components/data-table/blog-table.tsx
@@ -89,7 +89,7 @@ export const BlogTable = ({ data, onDelete, isPending }: Props) => {
             <Dialog open={!!editBlog} onOpenChange={(open) => !open && setEditBlog(null)}>
          
     <DialogContent
-                className="w-full max-w-[95vw] sm:max-w-3xl lg:max-w-5xl overflow-y-auto sm:rounded-lg"
+                className="w-full max-w-[95vw] sm:max-w-3xl lg:max-w-5xl overflow-y-auto h-full sm:rounded-lg"
               >
                 <DialogHeader>
                   <DialogTitle className='sr-only'>Edit Blog</DialogTitle>
